d1c234bdb2ce5901a21ab3fdee5a80990e1637ff
Andy Roberts <andy.roberts@macmillan.com>
2017-08-09 13:01:39 +0100

    show use of gather

--- dojo.py
+++ dojo.py
@@ -4,9 +4,16 @@ import asyncio
 async def requests_coroutine(name, url):
     print(name, requests.get(url))
 
+async def parallel_on_loop():
+    await asyncio.gather(
+            requests_coroutine("A", "http://www.google.com"),
+            requests_coroutine("B", "http://www.google.com"),
+            requests_coroutine("C", "http://www.google.com")
+            )
+
 loop = asyncio.get_event_loop()
 
-loop.run_until_complete(requests_coroutine("a", "http://www.google.com"))
+loop.run_until_complete(parallel_on_loop())
 
 loop.close()
 
