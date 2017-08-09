bde1f16b5508e1b1961013c483ba19977199d843
Andy Roberts <andy.roberts@macmillan.com>
2017-08-09 13:57:54 +0100

    make the http gets truly async

--- dojo.py
+++ dojo.py
@@ -1,15 +1,23 @@
 import requests
 import asyncio
+import aiohttp
+from aiohttp import ClientSession
 
 async def requests_coroutine(name, url):
     print(name, requests.get(url))
 
 async def parallel_on_loop():
     await asyncio.gather(
-            requests_coroutine("A", "https://httpbin.org/range/1024?duration=5"),
-            requests_coroutine("B", "http://www.google.com"),
-            requests_coroutine("C", "http://www.google.com")
+            get_file("A", "https://httpbin.org/range/1024?duration=15"),
+            get_file("B", "https://httpbin.org/range/1024?duration=10"),
+            get_file("C", "https://httpbin.org/range/1024?duration=5"),
             )
+
+async def get_file(name, url):
+    async with ClientSession() as session:
+        async with session.get(url) as response:
+            result = await response.read()
+            print(name, response.status)
 
 loop = asyncio.get_event_loop()
 
